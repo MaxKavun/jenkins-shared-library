498148cdfc43434574f46b3b1ec7689c03cd350a
Max <maxkavun@outlook.com>
2020-04-07 12:52:53 +0300

    Add regex to search options directive

--- vars/updateVersions.groovy
+++ vars/updateVersions.groovy
@@ -1,6 +1,19 @@
 #!/usr/bin/env groovy
 
+def addBuildDiscardOption() {
+    def optionsDirective = '''
+        options {
+            buildDiscarder(logRotator(numToKeepStr: '5'))
+        }
+    '''
+    def jenkinsFile = readFile "Jenkinsfile"
+    def matcher = jenkinsFile =~ /options.*{([^}]*)}/
+    assert matcher.find()
+    print matcher.size()
+}
+
 def createNewReleaseBranch() {
+    addBuildDiscardOption()
     sh "git branch release/${BUILD_NUMBER}"
     sh "git checkout release/${BUILD_NUMBER}"
     sshagent (['22aebe55-e3cf-48af-b4cc-0ca480a4fc77']) {
