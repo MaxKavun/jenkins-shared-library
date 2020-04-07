2a1896f6211941601ae8132059904bd2be628c69
Max <maxkavun@outlook.com>
2020-04-07 12:28:58 +0300

    Add credentials for git push command

--- vars/updateVersions.groovy
+++ vars/updateVersions.groovy
@@ -3,7 +3,9 @@
 def createNewReleaseBranch() {
     sh "git branch release/${BUILD_NUMBER}"
     sh "git checkout release/${BUILD_NUMBER}"
-    sh "git push origin release/${BUILD_NUMBER}"
+    sshagent (['22aebe55-e3cf-48af-b4cc-0ca480a4fc77']) {
+        sh "git push origin release/${BUILD_NUMBER}"
+    }
 }
 
 def call() {
