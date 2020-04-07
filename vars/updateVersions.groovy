8af18f87de6bd8650b17c13121e11be50e7e1da5
Max <maxkavun@outlook.com>
2020-04-07 14:26:46 +0300

    Move writing method into another method

--- vars/updateVersions.groovy
+++ vars/updateVersions.groovy
@@ -15,14 +15,16 @@ def addBuildDiscardOption(jenkinsFile) {
     if (!matcher) {
         def newJenkinsFile = jenkinsFile.replace('stages',optionsDirective)
         print newJenkinsFile
-        writeChangesToFile(newJenkinsFile)
+        return newJenkinsFile
     }
     print matcher.size()
 }
 
 def createNewReleaseBranch() {
     def jenkinsFile = readFile "Jenkinsfile"
-    addBuildDiscardOption(jenkinsFile)
+    def newJenkinsFile = addBuildDiscardOption(jenkinsFile)
+    if(newJenkinsFile)
+        writeChangesToFile(newJenkinsFile)
     sh "git branch release/${BUILD_NUMBER}"
     sh "git checkout release/${BUILD_NUMBER}"
     sshagent (['22aebe55-e3cf-48af-b4cc-0ca480a4fc77']) {
