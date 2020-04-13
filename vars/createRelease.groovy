cef45ec906932452762899d5d869137477a86953
Max <maxkavun@outlook.com>
2020-04-13 15:56:43 +0300

    Add param

--- vars/createRelease.groovy
+++ vars/createRelease.groovy
@@ -1,12 +1,12 @@
 #!/usr/bin/env groovy
 import com.max.jsl.*
 
-def call() {
+def call(buildSettings) {
     /*
       * Create release code with creating new branch and pushing to the SCM
       * Just for testing
     */
-    if (BuildSettings.getVersionedSemantically()) {
+    if (buildSettings.getVersionedSemantically()) {
         sh "git branch release/${BUILD_NUMBER}"
         sh "git checkout release/${BUILD_NUMBER}"
         sshagent (['22aebe55-e3cf-48af-b4cc-0ca480a4fc77']) {
