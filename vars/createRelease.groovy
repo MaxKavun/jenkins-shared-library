3367a4831809591847885b6d8b40e45646ae478c
Max <maxkavun@outlook.com>
2020-04-13 15:46:11 +0300

    Add CreateRelease method

---
+++ vars/createRelease.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/env groovy
+
+
+def call() {
+    /*
+      * Create release code with creating new branch and pushing to the SCM
+      * Just for testing
+    */
+    sh "git branch release/${BUILD_NUMBER}"
+    sh "git checkout release/${BUILD_NUMBER}"
+    sshagent (['22aebe55-e3cf-48af-b4cc-0ca480a4fc77']) {
+        sh "git push origin release/${BUILD_NUMBER}"
+    }
+}
