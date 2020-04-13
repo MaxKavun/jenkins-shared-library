33f1257448d95e29cf7597ff338cb6583596361e
Max <maxkavun@outlook.com>
2020-04-13 16:00:15 +0300

    Invert condition for createRelease method

--- vars/createRelease.groovy
+++ vars/createRelease.groovy
@@ -6,7 +6,7 @@ def call(buildSettings) {
       * Create release code with creating new branch and pushing to the SCM
       * Just for testing
     */
-    if (buildSettings.getVersionedSemantically()) {
+    if (!buildSettings.getVersionedSemantically()) {
         sh "git branch release/${BUILD_NUMBER}"
         sh "git checkout release/${BUILD_NUMBER}"
         sshagent (['22aebe55-e3cf-48af-b4cc-0ca480a4fc77']) {
