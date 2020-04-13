ede2396edb4f27414e6277df894a16f311b378e5
Max <maxkavun@outlook.com>
2020-04-13 15:53:37 +0300

    Add getter for versionedSemantically

--- src/com/max/jsl/BuildSettings.groovy
+++ src/com/max/jsl/BuildSettings.groovy
@@ -3,4 +3,8 @@ package com.max.jsl;
 class BuildSettings {
     private String branchName = null
     private Boolean versionedSemantically = false
+
+    public Boolean getVersionedSemantically() {
+        return versionedSemantically
+    }
 }
