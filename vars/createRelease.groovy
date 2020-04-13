25552f9424cf669003f5b8111ed609d2d80fa2b8
Max <maxkavun@outlook.com>
2020-04-13 15:54:55 +0300

    Add method instead of property

--- vars/createRelease.groovy
+++ vars/createRelease.groovy
@@ -6,7 +6,7 @@ def call() {
       * Create release code with creating new branch and pushing to the SCM
       * Just for testing
     */
-    if (BuildSettings.versionedSemantically) {
+    if (BuildSettings.getVersionedSemantically()) {
         sh "git branch release/${BUILD_NUMBER}"
         sh "git checkout release/${BUILD_NUMBER}"
         sshagent (['22aebe55-e3cf-48af-b4cc-0ca480a4fc77']) {
