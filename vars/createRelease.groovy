cf8d78c0c6fd117a7cfcf543c28e4e309dacd723
Max <maxkavun@outlook.com>
2020-04-13 15:51:07 +0300

    Add condition

--- vars/createRelease.groovy
+++ vars/createRelease.groovy
@@ -6,9 +6,11 @@ def call() {
       * Create release code with creating new branch and pushing to the SCM
       * Just for testing
     */
-    sh "git branch release/${BUILD_NUMBER}"
-    sh "git checkout release/${BUILD_NUMBER}"
-    sshagent (['22aebe55-e3cf-48af-b4cc-0ca480a4fc77']) {
-        sh "git push origin release/${BUILD_NUMBER}"
+    if (BuildSettings.versionedSemantically) {
+        sh "git branch release/${BUILD_NUMBER}"
+        sh "git checkout release/${BUILD_NUMBER}"
+        sshagent (['22aebe55-e3cf-48af-b4cc-0ca480a4fc77']) {
+            sh "git push origin release/${BUILD_NUMBER}"
+        }
     }
 }
