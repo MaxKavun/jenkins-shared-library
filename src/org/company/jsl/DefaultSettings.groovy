61347a054bc5a32ed4fe2dfb89696046e897e8d9
Max Kavun <maxkavun@outlook.com>
2020-12-24 15:04:29 +0300

    Upfsyr

--- src/org/company/jsl/DefaultSettings.groovy
+++ src/org/company/jsl/DefaultSettings.groovy
@@ -2,4 +2,5 @@ package org.company.jsl
 
 class DefaultSettings {
     public static final String SSH_CREDENTIALS_ID = 'jenkins-deployment'
+    public static final String DOCKER_REGISTRY = '172.17.0.1:5050'
 }
--- src/org/company/jsl/ci/Docker.groovy
+++ src/org/company/jsl/ci/Docker.groovy
@@ -1,24 +1,28 @@
 package org.company.jsl.ci
 
+import org.company.jsl.DefaultSettings
+
 class Docker implements Serializable {
     
     def config
     def dockerImageId
     def job
+    def dockerRegistry = DefaultSettings.DOCKER_REGISTRY
 
     Docker(job, config) {
         this.job = job
         this.config = config
+        this.dockerRegistry = this.config["dockerRegistry"] ?: this.dockerRegistry
     }
 
     def build() {
-        job.docker.withRegistry("http://172.17.0.1:5050", "nexus") {
-            this.dockerImageId = job.docker.build("172.17.0.1:5050/myimage:1.0").id
+        job.docker.withRegistry("http://${this.dockerRegistry}", "nexus") {
+            this.dockerImageId = job.docker.build("${this.dockerRegistry}/myimage:1.0").id
         }
     }
 
     def publish() {
-        job.docker.withRegistry("http://172.17.0.1:5050", "nexus") {
+        job.docker.withRegistry("http://${this.dockerRegistry}", "nexus") {
             def image = job.docker.image(this.dockerImageId)
             image.push()
             image.push("latest")
