42f17d1868f303e6f950d9189c776e0a8a96303f
Max Kavun <maxkavun@outlook.com>
2020-12-24 15:52:42 +0300

    Remove inverse

--- src/org/company/jsl/ci/Docker.groovy
+++ src/org/company/jsl/ci/Docker.groovy
@@ -19,7 +19,7 @@ class Docker implements Serializable {
             this.dockerRegistry = this.config["dockerRegistry"] ?: this.dockerRegistry
             this.imageName = this.config["imageName"]
             this.imageVersion = this.config["imageVersion"]
-            if (!this.imageName || !this.imageVersion) {
+            if (this.imageName || this.imageVersion) {
                 throw new RuntimeException('Required arguments are missed')
             }
         } catch(RuntimeException ex) {
